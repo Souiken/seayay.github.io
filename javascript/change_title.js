815a03c1f3f29716c38adb19956a5024049a26c7
Seayay <92374733+Seayay@users.noreply.github.com>
2022-06-26 15:58:12 +0800

    优化了文件目录, 移除了残留文件

---
+++ javascript/change_title.js
@@ -0,0 +1,13 @@
+document.addEventListener('visibilitychange', function () {
+    var isHidden = document.hidden;
+    if (isHidden) {
+      document.title = '你去看什么了(；′⌒`)';
+    } else {
+      setTimeout(() => {
+        document.title = '你回来了!q(≧▽≦q)';
+      }, 2000);
+      setTimeout(() => {
+        document.title = 'GitHub@Seayay';
+      }, 3000);
+    }
+  });
